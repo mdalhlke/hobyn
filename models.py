d5b66fc995148024df9b8ad37c39166ed3a113b9
Luis Garcia <luisfergar99@gmail.com>
2017-08-14 09:39:06 -0700

    img model+

--- models.py
+++ models.py
@@ -2,14 +2,23 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     name= ndb.StringProperty()
     username= ndb.StringProperty()
+    avatar= ndb.BlobProperty()
     date_created= ndb.DateProperty()
     friends= ndb.StringProperty()
     avatar= ImageProperty()
-class Hobby:
-    name= ndb.StringProperty()
-    description= ndb.StringProperty()
     N-points= ndb.IntProperty()
     O-points= ndb.IntProperty()
     A-points= ndb.IntProperty()
     C-points= ndb.IntProperty()
     E-points= ndb.IntProperty()
+    
+class Hobby:
+    name= ndb.StringProperty()
+    description= ndb.StringProperty()
+    image= ndb.BlobProperty()
+    N-points= ndb.IntProperty()
+    O-points= ndb.IntProperty()
+    A-points= ndb.IntProperty()
+    C-points= ndb.IntProperty()
+    E-points= ndb.IntProperty()
+    
