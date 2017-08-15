3ff5db7ab439653138fa4e08457f73464c5e7407
Merissa Tan <merissa030397@gmail.com>
2017-08-14 22:00:59 -0700

    removed unneeded script

--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,4 @@
 
-function displaySidebar() {
-    if($('#sidebar').css('display')== "none"){
-        $('#sidebar').css({display:'block'});
-        $('.column').css({width:'33.3%'});
-        $('#right').css({ left:'33.3%'});
-        $('.tap').css({right:'30%'});
-    }
-    else if($('#sidebar').css('display')== "block"){
-        $('#sidebar').css({display:'none'});
-        $('.column').css({width:'50%'})
-        $('#right').css({ left:'50%'})
-        $('.tap').css({right:'-50px'})
-    }
-}
-function setUp(){
-    $('.tap').on('click', displaySidebar);
-}
-
-var modal = document.getElementById('id01');
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
